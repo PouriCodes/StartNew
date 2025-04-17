5f6e0153b0b21c7447ab2635d39a8a2ae624bbbe
PouriCodes <100853066+PouriCodes@users.noreply.github.com>
2025-04-17 12:05:06 +0330

    New template DIR
    Added DIR of template folder

--- PouriaSite/settings.py
+++ PouriaSite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'PouriaSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Add your templates directory here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
