afd080e1bc01fb5fcc57b58605294bf6a61576d9
PouriCodes <100853066+PouriCodes@users.noreply.github.com>
2025-04-22 13:15:50 +0330

    Do Blog and Website migrate
    + blog
    + website
    migration

--- website/models.py
+++ website/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+class Contact(models.Model):     
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# SELECT * FROM post
+# SELECT * FROM post WHERE status = 1
