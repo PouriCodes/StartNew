bf7674f8ce268f5fa73aa62023a0d56210610409
PouriCodes <100853066+PouriCodes@users.noreply.github.com>
2025-04-24 14:42:45 +0330

    Dynamic pages
    + start to make pages dynamic

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ app_name = "blog"
 # from .views import index_view, about_view, contact_view  # Import your views here
 urlpatterns = [
     path("", blog_view, name="index"),  # Add this line to include the test view
-    path("single", blog_single, name="single"),  # Add this line to include the test view
+    path("<int:pid>", blog_single, name="single"),  # Add this line to include the test view
+    #path("post-<int:pid>", test, name="test"),  # Add this line to include the test view
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
 # Create your views here.
 def blog_view(request):
-    return render(request, "blog/blog-home.html")
-
-def blog_single(request):
+    posts = Post.objects.filter(status=1)
     context = {
-        'title': 'Forex Trading',
-        'content': 'This is the content of the blog forex page.',
-        'author': 'Pouria N',
-        'date': '2025-11-07',
+        'posts': posts,
+    }
+    return render(request, "blog/blog-home.html",context)
+
+def blog_single(request,pid):
+    post = get_object_or_404(Post,pk=pid)
+    context = {
+        'post':post
     }
     return render(request, "blog/blog-single.html",context)
+
+def test(request,pid):
+    #post = Post.objects.get(id=pid)
+    post = get_object_or_404(Post,pk=pid)
+    context = {
+        'post':post
+    }
+    return render(request, "test.html", context)
