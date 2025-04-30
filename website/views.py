1f7af13ffc552eced2be957dd7ebbe0b279a46ec
PouriCodes <100853066+PouriCodes@users.noreply.github.com>
2025-04-30 11:22:20 +0330

    Form
    + Form contact

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from website.models import Contact
+from website.forms import NameForm
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -15,17 +16,25 @@ def contact_view(request):
 
 
 def test_view(request):
+    form = NameForm()  # Initialize the form with no data
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        c = Contact()
-        c.name = name
-        c.email = email
-        c.subject = subject
-        c.message = message
-        c.save()
-        print(name,email,subject,message)
+        form = NameForm(request.POST)
+        if form.is_valid():
+            # Process the data in form.cleaned_data
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            # Save the data to the database or send an email, etc.
+            contact = Contact(name=name, email=email, subject=subject, message=message)
+            contact.save()
+
+            # Return a success response
+            print("Form is valid")
+        else:
+            # Return an error response
+            return HttpResponse("Form is not valid")
         
-    return render(request, 'test.html',{})
+    return render(request, 'test.html',{'form': form})
+
